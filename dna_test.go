dd149db321b2219d09a78fa6c12d9010cc6cda29
Rafe Colton <r.colton@modcloth.com>
2014-05-17 12:54:40 -0700

    Changing "ret" to "subject" in genome test

--- dna_test.go
+++ dna_test.go
@@ -23,17 +23,17 @@ var _ = Describe("NewGenome()", func() {
 	})
 
 	It("assigns token from the provided options", func() {
-		ret, _ := NewGenome(opts)
+		subject, _ := NewGenome(opts)
 
-		Expect(ret.APIToken).To(Equal("abc123"))
+		Expect(subject.APIToken).To(Equal("abc123"))
 	})
 
 	Context("with a non-integer depth", func() {
 		It("returns an error", func() {
 			opts["depth"] = "foo"
-			ret, err := NewGenome(opts)
+			subject, err := NewGenome(opts)
 
-			Expect(ret).To(BeNil())
+			Expect(subject).To(BeNil())
 			Expect(err).ToNot(BeNil())
 		})
 	})
@@ -41,9 +41,9 @@ var _ = Describe("NewGenome()", func() {
 	Context("with no account specified", func() {
 		It("returns an error", func() {
 			opts["account"] = ""
-			ret, err := NewGenome(opts)
+			subject, err := NewGenome(opts)
 
-			Expect(ret).To(BeNil())
+			Expect(subject).To(BeNil())
 			Expect(err).ToNot(BeNil())
 		})
 	})
@@ -51,27 +51,27 @@ var _ = Describe("NewGenome()", func() {
 	Context("with no repo specified", func() {
 		It("returns an error", func() {
 			opts["repo"] = ""
-			ret, err := NewGenome(opts)
+			subject, err := NewGenome(opts)
 
-			Expect(ret).To(BeNil())
+			Expect(subject).To(BeNil())
 			Expect(err).ToNot(BeNil())
 		})
 	})
 
 	Context("with an empty cache option", func() {
 		It("defaults the cache option to `no`", func() {
-			ret, _ := NewGenome(opts)
+			subject, _ := NewGenome(opts)
 
-			Expect(ret.UseCache).To(Equal("no"))
+			Expect(subject.UseCache).To(Equal("no"))
 		})
 	})
 
 	Context("with an invalid cache option", func() {
 		It("returns an error", func() {
 			opts["cache"] = "foo"
-			ret, err := NewGenome(opts)
+			subject, err := NewGenome(opts)
 
-			Expect(ret).To(BeNil())
+			Expect(subject).To(BeNil())
 			Expect(err).ToNot(BeNil())
 		})
 	})
@@ -79,9 +79,9 @@ var _ = Describe("NewGenome()", func() {
 	Context("with no ref specified", func() {
 		It("returns an error", func() {
 			opts["ref"] = ""
-			ret, err := NewGenome(opts)
+			subject, err := NewGenome(opts)
 
-			Expect(ret).To(BeNil())
+			Expect(subject).To(BeNil())
 			Expect(err).ToNot(BeNil())
 		})
 	})
