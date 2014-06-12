87b99c15a8991c1703eb2792d04d9450e7a2c463
Rafe Colton <r.colton@modcloth.com>
2014-06-12 16:59:23 -0700

    Adding logrus, using a byte buffer to capture exec.Cmd stdout

---
+++ log.go
@@ -0,0 +1,18 @@
+package kamino
+
+import "github.com/Sirupsen/logrus"
+
+var logger = logrus.New()
+
+func init() {
+	logger.Formatter = &logrus.JSONFormatter{}
+	logger.Level = logrus.Info // default to Info
+}
+
+/*
+ SetLogLevel sets the log level (from Sirupsen/logrus) on kamino's internal
+ logger
+*/
+func SetLogLevel(level logrus.Level) {
+	logger.Level = level
+}
